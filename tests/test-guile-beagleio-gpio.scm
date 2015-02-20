743c5a5f335ffd5229ff78de7e5ad033937206b8
Chad Albers <calbers@neomantic.com>
2015-02-20 11:59:10 +0000

    small set of tests for export and lookup

--- tests/test-guile-beagleio-gpio.scm
+++ tests/test-guile-beagleio-gpio.scm
@@ -7,42 +7,40 @@
 
 
 (test-begin "gpio-number-lookup")
-(define-syntax test-gpio-numbers
-  (syntax-rules (=>)
-    ((_ desc (name => pin) ...)
-     (test-group
-      desc
-      (test-eq pin (gpio-number-lookup name))
-      ...))))
-(test-gpio-numbers
- "(gpio-number-lookup name)"
- ("USR0" => 53)
- ("USR1" => 54)
- ("USR2" => 55)
- ("USR3" => 56)
- ;; P8
- ;;("P8_1" => 0)
- ;;("P8_2" => 0)
- ("P8_3" => 38)
- ("P8_4" => 39)
- ("P8_5" => 34)
- ("P8_6" => 35)
 
- ("P9_16" => 51)
- ("P9_16" => 51))
+(test-eq 53 (gpio-number-lookup "USR0"))
+(test-eq 54 (gpio-number-lookup "USR1"))
+(test-eq 55 (gpio-number-lookup "USR2"))
+(test-eq 56 (gpio-number-lookup "USR3"))
+(test-eq 38 (gpio-number-lookup "P8_3"))
+(test-eq 39 (gpio-number-lookup "P8_4"))
+(test-eq 34 (gpio-number-lookup "P8_5"))
+(test-eq 35 (gpio-number-lookup "P8_6"))
+(test-eq 50 (gpio-number-lookup "P9_14"))
+(test-eq 51 (gpio-number-lookup "P9_16"))
 
 (test-end "gpio-number-lookup")
 
-
 (test-begin "gpio-setup")
-(test-error 'gpio-error (gpio-setup "Hello"))
+(test-error 'gpio-error (gpio-setup "non existing name"))
 
-(map (lambda (name)
-       (test-assert
-	(let ((pin (gpio-number-lookup name)))
-	  (gpio-setup name)
-	  (access? (string-append "/sys/class/gpio/gpio" (number->string pin)) F_OK)
-	  #t)))
-     '("P9_16"))
+(define-syntax test-gpio-sysfs-export
+  (syntax-rules ()
+    ((_ (proc name) ...)
+     (test-group
+      "testing gpio-setup export to sysfs"
+      (test-assert
+       (let ((pin (gpio-number-lookup name)))
+	 (apply proc (list name))
+	 (access? (string-append "/sys/class/gpio/gpio" (number->string pin)) F_OK)
+	  #t)) ...))))
+
+(test-gpio-sysfs-export
+ (gpio-setup "P8_3")
+ (gpio-setup "P8_4")
+ (gpio-setup "P8_5")
+ (gpio-setup "P8_6")
+ (gpio-setup "P9_14")
+ (gpio-setup "P9_16"))
 
 (test-end "gpio-setup")
